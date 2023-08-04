54a95e873c4ebf57260127fd3e9d578317501d9e
Jackson5566 <92239015+Jackson5566@users.noreply.github.com>
2023-08-04 10:38:50 +0200

    Arreglo de esaticos

--- api/settings.py
+++ api/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'api.urls'
@@ -139,6 +140,8 @@ STATIC_ROOT =  BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
