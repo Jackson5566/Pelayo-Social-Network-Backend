7cd454759fb13c845f8c085e8cb09222c01db67e
Jackson5566 <92239015+Jackson5566@users.noreply.github.com>
2023-08-04 11:16:27 +0200

    Nueva a

--- api/settings.py
+++ api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&^_21q!(yq3_3ygyu)nkou^8@vyab-+!!0y^$6o_sl+=*m2tk0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
