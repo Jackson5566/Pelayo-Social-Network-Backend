36a3cd11745a7d4aa83af8feea85d3cb39c26b8b
Jackson5566 <92239015+Jackson5566@users.noreply.github.com>
2023-08-05 14:10:45 +0200

    Ahora si

--- auth_app/classes/denunciate.py
+++ auth_app/classes/denunciate.py
@@ -16,18 +16,21 @@ class DenunciateUser:
             return Response({'message': 'Usuario no encontrado'}, status=status.HTTP_400_BAD_REQUEST)
 
     def start_denunciate_proccess(self):
-        self.verify_complain()
-        self.add_complaint()
-        user_reported_denuncations = self.count_user_reported_complaints()
-        if user_reported_denuncations >= 20:
-            self.send_complaint_mail(user_reported_denuncations=user_reported_denuncations)
-        return Response("Usario denunciado", status=status.HTTP_200_OK)
+        complain_error = self.verify_complain()
+        if complain_error:
+            return complain_error
+        else:   
+            self.add_complaint()
+            user_reported_denuncations = self.count_user_reported_complaints()
+            if user_reported_denuncations >= 20:
+                self.send_complaint_mail(user_reported_denuncations=user_reported_denuncations)
+            return Response("Usario denunciado", status=status.HTTP_200_OK)
 
     def verify_complain(self) -> Union[Response, None]:
         if self.user_who_reported not in self.user_reported.denunciations.all():
             if self.user_who_reported == self.user_reported:
                 return Response({'message', 'No puedes denunciarte'}, status=status.HTTP_400_BAD_REQUEST)
-        else: return Response('Usuario ya denunciado')
+        else: return Response('Usuario ya denunciado', status=status.HTTP_400_BAD_REQUEST)
     
     def add_complaint(self):
         self.user_reported.denunciations.add(self.user_who_reported)
