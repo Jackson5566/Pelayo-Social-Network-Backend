761cf208b8d9c1bcc01d6404282cf468e510e5d6
Jackson5566 <92239015+Jackson5566@users.noreply.github.com>
2023-08-05 14:03:10 +0200

    Arreglo total

--- auth_app/classes/denunciate.py
+++ auth_app/classes/denunciate.py
@@ -2,6 +2,7 @@ from ..models import User
 from rest_framework.response import Response
 from django.core.mail import send_mail
 from rest_framework import status
+from typing import Union
 
 class DenunciateUser:
     def __init__(self, user_who_reported, user_reported_id):
@@ -15,22 +16,18 @@ class DenunciateUser:
             return Response({'message': 'Usuario no encontrado'}, status=status.HTTP_400_BAD_REQUEST)
 
     def start_denunciate_proccess(self):
-        try:
-            self.verify_complain()
-        except Exception as err:
-            return Response({'message', str(err)}, status=status.HTTP_400_BAD_REQUEST)
-        
+        self.verify_complain()
         self.add_complaint()
         user_reported_denuncations = self.count_user_reported_complaints()
         if user_reported_denuncations >= 20:
             self.send_complaint_mail(user_reported_denuncations=user_reported_denuncations)
         return Response("Usario denunciado", status=status.HTTP_200_OK)
 
-    def verify_complain(self):
+    def verify_complain(self) -> Union[Response, None]:
         if self.user_who_reported not in self.user_reported.denunciations.all():
             if self.user_who_reported == self.user_reported:
-                raise Exception('No te puedes denunciar a ti mismo')
-        else: return Exception('Usuario ya denunciado')
+                return Response({'message', 'No puedes denunciarte'}, status=status.HTTP_400_BAD_REQUEST)
+        else: return Response('Usuario ya denunciado')
     
     def add_complaint(self):
         self.user_reported.denunciations.add(self.user_who_reported)
@@ -42,36 +39,3 @@ class DenunciateUser:
     def send_complaint_mail(self, user_reported_denuncations):
         send_mail("Exceso de denuncias", f"El usuario ha sido denunciado {user_reported_denuncations} veces", 
                   None,  ["jackson0102almeida@gmail.com"])
-
-
-
-
-"""
-try:
-      user_id = request.data['id']
-      user = User.objects.filter(id=user_id).first()
-
-      if user not in user.denunciations.all():
-        if user != request.user:
-          user.denunciations.add(request.user)
-
-          user_denuncations = len(user.denunciations.all())
-          if user_denuncations >= 20:
-              send_mail("Exceso de denuncias", f"El usuario ha sido denunciado {user_denuncations} veces", None,  ["jackson0102almeida@gmail.com"])
-
-          return Response({
-              'message': 'Usuario denunciado'
-          }, status=status.HTTP_200_OK)
-        else:
-            return Response({
-                'message': 'No te puedes denunciar a ti mismo'
-            })
-      else:
-          return Response({
-              'message': 'Usuario ya denunciado'
-          })
-    
-    except User.DoesNotExist:
-        return Response({
-            'message': 'Usuario no encontrado'
-        })"""
